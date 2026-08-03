e993ca098e1ac2e769944cf062a83890215e2492
mahekghodmare <mahek.ghodmare.batch2024@sitnagpur.siu.edu.in>
2026-08-03 16:47:24 +0530

    Add files via upload

---
+++ input.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+// Single line comment
+
+int main()
+{
+    int a=10;
+    int b=20;
+
+    /* Multi-line
+       comment */
+
+    int sum=a+b;
+
+    if(sum>20)
+    {
+        printf("Sum = %d",sum);
+    }
+
+    return 0;
+}
